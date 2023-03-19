52da7c632503dcfcecdce63d415f8f44b3eacf45
micha <michel.plainchamp@gmail.com>
2023-03-19 22:21:37 +0100

    cleaned up all models and inserted Lombok in all of them, namely the annotations @Data, @Builder, @NoArgsConstructor, @AllArgsConstructor

--- src/main/java/com/trading/app/demo/model/Profile.java
+++ src/main/java/com/trading/app/demo/model/Profile.java
@@ -2,16 +2,20 @@ package com.trading.app.demo.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table
 public class Profile {
 
-
     //FIELDS
-
     @Id
     @SequenceGenerator(
             name = "profile_sequence",
@@ -39,85 +43,4 @@ public class Profile {
             )
     )
     private User user;
-
-
-    //CONSTRUCTORS
-    public Profile() {
-    }
-
-    public Profile(Long id, String firstName, String lastName, String address, String phoneNumber) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Profile(String firstName, String lastName, String address, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-    }
-
-    //GETTERS AND SETTERS
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Profile{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", user=" + user +
-                '}';
-    }
 }
--- src/main/java/com/trading/app/demo/model/Wire.java
+++ src/main/java/com/trading/app/demo/model/Wire.java
@@ -2,9 +2,15 @@ package com.trading.app.demo.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Wire {
 
@@ -24,77 +30,9 @@ public class Wire {
     private long amount;
     private boolean withdrawal;
 
-//    @Column(
-//            name = "created_at",
-//            nullable = false,
-//            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
-//    )
-//    private LocalDateTime createdAt;
-//
-
-
     // RELATIONSHIPS
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    //CONSTRUCTORS
-    public Wire(){}
-
-    public Wire(long id, long amount, boolean withdrawal, User user) {
-        this.id = id;
-        this.amount = amount;
-        this.withdrawal = withdrawal;
-        this.user = user;
-    }
-
-    public Wire(long amount, boolean withdrawal, User user) {
-        this.amount = amount;
-        this.withdrawal = withdrawal;
-        this.user = user;
-    }
-
-    // GETTERS AND SETTERS
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(long amount) {
-        this.amount = amount;
-    }
-
-    public boolean isWithdrawal() {
-        return withdrawal;
-    }
-
-    public void setWithdrawal(boolean withdrawal) {
-        this.withdrawal = withdrawal;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Wire{" +
-                "id=" + id +
-                ", amount=" + amount +
-                ", withdrawal=" + withdrawal +
-                ", user=" + user +
-                '}';
-    }
 }
