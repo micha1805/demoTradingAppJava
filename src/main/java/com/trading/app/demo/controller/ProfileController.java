26ed38d9b68ec5da827845f07c36b6692b9382be
micha <michel.plainchamp@gmail.com>
2023-03-22 10:09:23 +0100

    found and easier way to read the requests and send the response

--- src/main/java/com/trading/app/demo/controller/ProfileController.java
+++ src/main/java/com/trading/app/demo/controller/ProfileController.java
@@ -5,10 +5,14 @@ import com.trading.app.demo.model.Profile;
 import com.trading.app.demo.model.User;
 import com.trading.app.demo.service.ProfileService;
 import com.trading.app.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -20,9 +24,9 @@ public class ProfileController {
     private final UserService userService;
 
     @GetMapping
-    public ResponseEntity<FullProfileResponse> getProfile( @RequestHeader("Authorization") String authHeader){
+    public ResponseEntity<FullProfileResponse> getProfile(@RequestHeader("Authorization") String authHeader) {
 
-      // should take care of the case when no user is found:
+        // should take care of the case when no user is found:
         User user = userService.getUserFromHeader(authHeader);
         FullProfileResponse response;
         Profile profile = profileService.findByUserId(user.getId());
@@ -47,7 +51,19 @@ public class ProfileController {
         User currentUser = userService.getUserFromHeader(authHeader);
         System.out.println(currentUser.toString());
         System.out.println(currentUser.getId());
-        return ResponseEntity.ok( authHeader + requestBody);
+        return ResponseEntity.ok(authHeader + requestBody);
     }
 
+    // Another way to send json and read request body, less formal doesn't use any response
+    // class, just a Map.
+    @GetMapping(path = "/test2")
+    public ResponseEntity<Map<String, String>> test2(@RequestBody Map<String, Object> transactionMap) {
+
+        // get the value associated to the "hello" key in the request's body :
+        String hello = (String) transactionMap.get("hello");
+
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("myKey", hello);
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
+    }
 }
